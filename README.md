bce01db92cacc1abaedf0d1c798793f79cfc6065
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2025-09-28 20:37:22 +0200

    preping for wasm

---
+++ README.md
@@ -0,0 +1,6 @@
+### Cross 
+Cross platform build for Linux, Mac, Windows, Web, Android and iOS
+
+### Why?
+R&D project for future contract, performance benchmarking
+
--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+//754/ We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct Cross {
